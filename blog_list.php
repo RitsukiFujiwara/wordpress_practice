53f2b804fc00b621a9c216cf21541d032a910847
藤原立暉 <r9.20important@icloud.com>
2020-08-02 14:34:54 +0900

    no message

---
+++ blog_list.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>サンプルホームページ</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<header class="site-width">
+			<h1><a href="index.html">WEBUKATU</a></h1>
+			<nav id="top-nav">
+				<ul>
+					<li><a href="index.html">HOME</a></li>
+					<li><a href="http://webukatu.com/sample/html_practice/#about">ABOUT</a></li>
+					<li><a href="http://webukatu.com/sample/html_practice/#merit">MERIT</a></li>
+					<li><a href="http://webukatu.com/sample/html_practice/#recruit">RECRUIT</a></li>
+					<li><a href="contact.html">CONTACT</a></li>
+					<li><a href="map.html">INFO</a></li>
+					<li><a href="blog_list.html">BlOG</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="main">
+
+			<!-- blog_list -->
+			<?php get_sidebar();?>
+					
+			</section>
+
+
+		</div>
+		<footer>
+			Copyright <a href="http://webukatu.com/">ウェブカツ!!サンプルホームページ</a>. All Rights Reserved.
+		</footer>
+	</body>
+</html>
